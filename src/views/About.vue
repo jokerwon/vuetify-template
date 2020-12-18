714771a5a888565f232d6e29cda064ae27acc8b4
wengjiankai <wengjiankai@zeaho.com>
2020-12-18 09:13:09 +0800

    feat: 修改关于页文案

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container>
     <v-avatar color="#1565C0">
-      <span class="white--text headline">JokerWon</span>
+      <span class="white--text headline">LM</span>
+      <span>Nice to meet you.</span>
     </v-avatar>
   </v-container>
 </template>
